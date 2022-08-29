af1b7bcf67cccdba37b23bd7f943bdec92069dc1
RhodaAdigun <ifeoluwa.adigun2102@gmail.com>
2022-08-29 15:07:59 +0000

    simple shell

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* memory.c*/
+void *_realloc(void *ptr, size_t oldsize, size_t newsize);
+void _memcpy(void *new_ptr, void *ptr, size_t size);
+
+#endif
