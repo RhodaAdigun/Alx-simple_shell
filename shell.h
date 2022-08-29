c9a87e780007c4660948e6c96eedae7230c051ab
RhodaAdigun <106974772+RhodaAdigun@users.noreply.github.com>
2022-08-29 18:19:55 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -8,4 +8,7 @@
 void *_realloc(void *ptr, size_t oldsize, size_t newsize);
 void _memcpy(void *new_ptr, void *ptr, size_t size);
 
+/* _getline.c*/
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
+
 #endif
