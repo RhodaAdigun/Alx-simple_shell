4485811b3e91f4469bc7e7265004e5fd2f4ce733
RhodaAdigun <106974772+RhodaAdigun@users.noreply.github.com>
2022-09-03 10:33:35 +0100

    Linked list

--- lists.c
+++ lists.c
@@ -1,16 +1,17 @@
 #include "shell.h"
 /**
  * add_node - Adds a new node at the beginning
- *   *            of a list_t list.
- *    * @head: A pointer to the head of the list_t list.
- *     * @str: The string to be added to the list_t list.
- *      *
- *       * Return: If the function fails - NULL.
- *        *         Otherwise - the address of the new element.
- *         */
-list_t *add_node(list_t **head, const char *str)
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+
+line_t *add_line_node(line_t **head, const char *str)
 {
-	list_t *new;
+	line_t *new;
 	char *dup_str;
 	int len;
 
@@ -18,18 +19,14 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 
-	dup_str = strdup(str);
+	dup_str = _strdup(str);
 	if (dup_str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup_str;
-	new->len = len;
+	new->line = dup_str;
 	new->next = *head;
 
 	*head = new;
@@ -47,7 +44,7 @@ list_t *add_node(list_t **head, const char *str)
  *         Otherwise - the address of the new element.
  */
 
-sep_t *add_node_end(sep_t **head, const char *str, int index)
+sep_t *add_sep_node(sep_t **head, const char *str, int index)
 {
 	char *dup;
 	int len;
@@ -64,7 +61,7 @@ sep_t *add_node_end(sep_t **head, const char *str, int index)
 		return (NULL);
 	}
 
-	new->str = dup;
+	new->separator = dup;
 	new->index = index
 	new->next = NULL;
 
@@ -84,13 +81,13 @@ sep_t *add_node_end(sep_t **head, const char *str, int index)
 
 
 /**
- * free_list - Frees a list_t list.
- * @head: A pointer to the list_t list.
+ * free_list - Frees a line_t list.
+ * @head: A pointer to the line_t list.
  */
 
-void free_list(list_t *head)
+void free_line_list(line_t *head)
 {
-	list_t *tmp;
+	line_t *tmp;
 
 	while (head)
 	{
